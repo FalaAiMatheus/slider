534219ad33bf21c52adbaeec3894a79c97d0a18b
matheus-franca <matheussfrancads2345@gmail.com>
2024-09-15 20:49:07 -0300

    feat: create card component

---
+++ src/components/Card/card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="max-w-sm flex flex-col space-y-2 p-4 bg-slate-50 border border-slate-200 rounded-lg shadow-sm"
+  >
+    <h5 class="text-2xl font-bold tracking-tight text-slate-900">
+      {{ slideTitle }}
+    </h5>
+    <span class="text-slate-400 text-sm">Data: {{ date }}</span>
+    <span class="text-slate-400 text-sm">Local: {{ locale }}</span>
+    <p class="text-slate-700">
+      {{ description }}
+    </p>
+    <RouterLink
+      to="/slides"
+      class="px-3 py-2 text-sm font-medium self-start text-center text-slate-50 bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+    >
+      Ver Slide
+    </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  slideTitle: String,
+  date: Date,
+  locale: String,
+  description: String,
+});
+</script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,3 +1,26 @@
+<script setup>
+import Card from "../../components/Card/card.vue";
+import Header from "../../components/header.vue";
+</script>
+
 <template>
-  <h1>Home</h1>
+  <Header />
+  <section class="flex justify-center flex-wrap max-w-screen-xl gap-6 p-4">
+    <Card
+      slideTitle="Desenrolando Frameworks Node: Qual escolher?"
+      :date="actualDate"
+      locale="Digital College Aldeota"
+      description="Express, Fastify, Nest e Adonis. Afinal, qual framework escolher para sua aplicação Node ?"
+      v-for="(_, index) in card"
+      :key="index"
+    />
+  </section>
 </template>
+
+<script>
+import { ref } from "vue";
+const card = ref(Array.from({ length: 45 }));
+const actualDate = ref(
+  new Date().toLocaleDateString("pt-BR", { timeZone: "UTC" })
+);
+</script>
