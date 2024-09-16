6b2c3e6d1fd8fc3254fd3b6fc7bbacc3b4cff25b
matheus-franca <matheussfrancads2345@gmail.com>
2024-09-16 08:33:04 -0300

    feat: add dynamic route

--- src/components/Card/card.vue
+++ src/components/Card/card.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    :key="id"
     class="max-w-sm flex flex-col space-y-2 p-4 bg-slate-50 border border-slate-200 rounded-lg shadow-sm"
   >
     <h5 class="text-2xl font-bold tracking-tight text-slate-900">
@@ -16,20 +17,21 @@
     <p class="text-slate-700">
       {{ description }}
     </p>
-    <RouterLink
-      to="/slides"
+    <router-link
+      :to="{ name: 'slides', params: { id: id } }"
       class="px-3 py-2 text-sm font-medium self-start text-center text-slate-50 bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus: ring-offset-blue-600 transition-all"
     >
       Ver Slide
-    </RouterLink>
+    </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Calendar, MapPinned } from "lucide-vue-next";
 defineProps({
+  id: Number,
   slideTitle: String,
-  date: Date,
+  date: String,
   locale: String,
   description: String,
 });
