f409d3cd8e19ad2e04a4b87e5b1a64fbc2a07f8c
matheus-franca <matheussfrancads2345@gmail.com>
2024-09-16 08:32:33 -0300

    feat: add card props type

---
+++ src/core/types/index.ts
@@ -0,0 +1,10 @@
+export type CardProps = {
+  created_at: string | null;
+  date: string;
+  description: string;
+  id: number;
+  locale: string;
+  name: string;
+  slug: string | null;
+  updated_at: string | null;
+};
