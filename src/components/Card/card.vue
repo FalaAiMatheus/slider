9fefbf2a2c583ddb59e6d4795a58cdad8848a4d2
matheus-franca <matheussfrancads2345@gmail.com>
2024-09-15 21:01:04 -0300

    refactor: add icons in card component

--- src/components/Card/card.vue
+++ src/components/Card/card.vue
@@ -5,14 +5,20 @@
     <h5 class="text-2xl font-bold tracking-tight text-slate-900">
       {{ slideTitle }}
     </h5>
-    <span class="text-slate-400 text-sm">Data: {{ date }}</span>
-    <span class="text-slate-400 text-sm">Local: {{ locale }}</span>
+    <div class="flex gap-1 items-center text-slate-400 text-sm">
+      <Calendar />
+      <span>{{ date }}</span>
+    </div>
+    <div class="flex gap-1 items-center text-slate-400 text-sm">
+      <MapPinned />
+      <span>{{ locale }}</span>
+    </div>
     <p class="text-slate-700">
       {{ description }}
     </p>
     <RouterLink
       to="/slides"
-      class="px-3 py-2 text-sm font-medium self-start text-center text-slate-50 bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+      class="px-3 py-2 text-sm font-medium self-start text-center text-slate-50 bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus: ring-offset-blue-600 transition-all"
     >
       Ver Slide
     </RouterLink>
@@ -20,6 +26,7 @@
 </template>
 
 <script setup lang="ts">
+import { Calendar, MapPinned } from "lucide-vue-next";
 defineProps({
   slideTitle: String,
   date: Date,
